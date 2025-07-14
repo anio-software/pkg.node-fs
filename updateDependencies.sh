00c07009c57775fc113e1d0632f7a3d192606b81
eXory2024 <exory@anio.software>
2025-07-14 03:00:08 +0200

    Add updateDependencies.sh.

---
+++ updateDependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eufx
+
+npm install --save-dev @anio-software/pkg.node-fs-copy@latest \
+@anio-software/pkg.node-fs-file@latest \
+@anio-software/pkg.node-fs-is@latest \
+@anio-software/pkg.node-fs-mkdir@latest \
+@anio-software/pkg.node-fs-path-type@latest \
+@anio-software/pkg.node-fs-remove@latest \
+@anio-software/pkg.node-fs-resolve@latest \
+@anio-software/pkg.node-fs-scandir@latest \
+@anio-software/pkg.node-fs-stat-path@latest \
+@anio-software/pkg.node-fs-tmp@latest
